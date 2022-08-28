c80c6eacf01fbbffcb3429d13b4b5a50b3e1e9ac
Wajahat43 <aliwajahat44@yahoo.com>
2022-08-28 14:34:56 +0500

    Added Comments in Assignment 3

--- Assignment3/Question3.js
+++ Assignment3/Question3.js
@@ -3,3 +3,33 @@
 //This program checks if sum of factorial of digtis of a number is equal to the number itself
 "use strict"
 
+
+//Calculates and returns factorial of a number
+function getFactorial(num){
+    let factorial = 1;
+    for(let i = 1; i <= num;i++){
+        factorial *= i;
+    }
+    return factorial;
+}
+
+//Checks if sum of factorial of digits of a number is equal to number itself.
+function isSpecial(num){
+    let numCopy = num
+    let sumFactorial = 0
+    while(numCopy > 0){
+        let digit = numCopy %10;
+        sumFactorial += getFactorial(digit)
+        numCopy = parseInt(numCopy / 10)
+    }
+
+    if(sumFactorial === num){
+        console.log(num + " is a special number.")
+    }
+    else{
+        console.log(num + " is not a special number.")
+    }
+
+}
+
+isSpecial(145)
